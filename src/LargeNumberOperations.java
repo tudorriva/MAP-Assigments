138bd9ce44437218c0c29b8a8e8f3c22f1947b4a
tudorriva <tudorivanceastefan@gmail.com>
2024-10-08 22:40:02 +0300

    Added Aufgabe 3 3 and 4 + tests

--- src/LargeNumberOperations.java
+++ src/LargeNumberOperations.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
+
 public class LargeNumberOperations {
 
     public int[] add(int[] num1, int[] num2) {
         int length = num1.length;
-        int[] result = new int[length];
+        int[] result = new int[length + 1];
         int carry = 0;
 
-        for(int i = length - 1; i>= 0; i++) {
+        for(int i = length - 1; i>= 0; i--) {
             int sum = num1[i] + num2[i] + carry;
-            result[i+1] = sum % 10;
+            result[i + 1] = sum % 10;
             carry = sum / 10;
         }
         result[0] = carry;
@@ -19,7 +21,7 @@ public class LargeNumberOperations {
         int[] result = new int[length];
         int borrow = 0;
 
-        for(int i = length - 1; i>= 0; i++) {
+        for(int i = length - 1; i>= 0; i--) {
             int diff = num1[i] - num2[i] - borrow;
             if(diff < 0) {
                 diff = diff + 10;
@@ -28,6 +30,37 @@ public class LargeNumberOperations {
                 borrow = 0;
             }
             result[i] = diff;
+        }
+        return result;
+    }
+
+    public int[] multiply(int[] num, int digit) {
+        int length = num.length;
+        int[] result = new int[length + 1];
+        int carry = 0;
+
+        for (int i = length - 1; i >= 0; i--) {
+            int product = num[i] * digit + carry;
+            result[i + 1] = product % 10;
+            carry = product / 10;
+        }
+        if (carry != 0) {
+            result[0] = carry;
+            return result;
+        } else {
+            return Arrays.copyOfRange(result, 1, result.length);
+        }
+    }
+
+    public int[] divide(int[] num, int divisor) {
+        int length = num.length;
+        int[] result = new int[length];
+        int remainder = 0;
+
+        for(int i = 0; i < length; i++) {
+            int dividend = remainder * 10 + num[i];
+            result[i] = dividend / divisor;
+            remainder = dividend % divisor;
         }
         return result;
     }
--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,17 @@ public class Main {
         System.out.println("Minimale Zahl: " + arrayOperations.findMin(numbers));
         System.out.println("Maximale Summe: " + arrayOperations.findMaxSum(numbers));
         System.out.println("Minimale Summe: " + arrayOperations.findMinSum(numbers));
+
+        // Aufgabe 3
+        LargeNumberOperations largeNumberOperations = new LargeNumberOperations();
+        int[] num1 = {1, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] num2 = {8, 7, 0, 0, 0, 0, 0, 0, 0};
+        int[] num3 = {8, 3, 0, 0, 0, 0, 0, 0, 0};
+        int[] num4 = {5, 4, 0, 0, 0, 0, 0, 0, 0};
+        int[] num5 = {2, 3, 6, 0, 0, 0, 0, 0, 0};
+        System.out.println("Addition von num1 und num2: " + Arrays.toString(largeNumberOperations.add(num1, num2)));
+        System.out.println("Subtraktion von num3 und num4: " + Arrays.toString(largeNumberOperations.subtract(num3, num4)));
+        System.out.println("Multipilkation von num5 mit 2: " + Arrays.toString(largeNumberOperations.multiply(num5, 2)));
+        System.out.println("Division von num5 durch 2: " + Arrays.toString(largeNumberOperations.divide(num5, 2)));
     }
 }
