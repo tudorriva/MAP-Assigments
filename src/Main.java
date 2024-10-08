6f0b05d9fd7cda5a9042caa532a111329a1f5929
tudorriva <tudorivanceastefan@gmail.com>
2024-10-08 21:02:17 +0300

    Added 1, 2 and 3 from Aufgabe 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Aufgabe 1
+        UniversityGrading gradingSystem = new UniversityGrading();
+        int[] grades = {15, 39, 40, 41, 84, 99, 55};
+
+        int[] failingGrades = gradingSystem.getFailingGrades(grades);
+        System.out.println("Nicht asureichende Noten: " + Arrays.toString(failingGrades));
+        System.out.println("Durschnittswert: " + gradingSystem.calculateAverage(grades));
+        System.out.println("Abgerundete Noten: " + Arrays.toString(gradingSystem.roundGrades(grades)));
     }
 }
--- src/UniversityGrading.java
+++ src/UniversityGrading.java
@@ -17,4 +17,34 @@ public class UniversityGrading {
         }
         return failingGrades;
     }
+
+    public double calculateAverage(int[] grades) {
+        double sum = 0;
+        for (int grade : grades) {
+            sum += grade;
+        }
+
+        return sum / grades.length;
+    }
+
+    public int[] roundGrades(int[] grades) {
+        int[] roundedGrades = new int[grades.length];
+        for(int i = 0; i< grades.length; i++)
+        {
+            int grade = grades[i];
+            if( grade < 38) {
+                roundedGrades[i] = grade;
+            } else {
+                int nextMultipleOfFive = ((grade / 5) + 1) * 5;
+                if(nextMultipleOfFive - grade < 3) {
+                    roundedGrades[i] = nextMultipleOfFive;
+                }
+                else {
+                    roundedGrades[i] = grade;
+                }
+            }
+        }
+
+        return roundedGrades;
+    }
 }
